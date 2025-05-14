f463bf1b7ca8984e7a8b468d2257511cb09586cc
Prabin Dangol <dprabin23@gmail.com>
2025-05-14 08:29:45 +0545

    login page layout
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import React from 'react'
+import placeholder from '../../assets/images/auth-hero.jpg'
+import bg from '../../assets/images/auth-bg.png'
+
+function AuthLayout({children}) {
+  return (
+    <section className='flex justify-center py-20 max-w-screen mx-auto'>
+      <Image  src={bg} alt='image' 
+      height={2000} 
+      width={2000} 
+      className='h-auto w-full fixed top-0 left-0 -z-1 opacity-50'/>
+      
+      <div className='grid grid-cols-1 md:grid-cols-2 w-full md:w-1/2 shadow-blue-100 rounded-lg p-2 bg-white min-h-[50vh]'>
+        {children}
+        <Image  src={placeholder} alt='image' height={1200} width={1200} className='h-full object-cover w-auto rounded-r-md'/>
+      </div>
+    </section>
+  )
+}
+
+export default AuthLayout;
---
+++ src/app/(auth)/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loginpage() {
+  return (
+    <div>
+      login page
+    </div>
+  )
+}
+
+export default Loginpage;
