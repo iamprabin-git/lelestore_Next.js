de4a42d65a7a1f7812a24c5d1dc45a3f44bca28a
Prabin Dangol <dprabin23@gmail.com>
2025-05-17 18:07:21 +0545

    profile image
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/components/AuthUserPopup.js
+++ src/components/AuthUserPopup.js
@@ -9,11 +9,11 @@ function AuthUserPopup({user}) {
   return (
       <div className="absolute top-10 right-0 bg-white shadow-lg p-4">
         <h4 className="whitespace-nowrap font-medium text-lg">Hi!, {user.name || "User"}</h4>
-        <Link href="/profile" className="flex items-center gap-2" onClick={() => setShowpopup(false)}>Profile</Link>
+        <Link href="/profile" className="items-center border rounded-lg px-2 py-1 hover:bg-slate-100 " onClick={() => setShowpopup(false)}>Profile</Link>
         
       <button
         onClick={() => dispatch(logoutUser())}
-        className="bg-slate-700 py-1 px-4 text-white rounded flex items-center gap-2"
+        className="bg-slate-700 py-1 px-4 text-white rounded flex items-center gap-2 mt-2"
       >
         
         <span onClick={() => setShowpopup(false)}>Logout</span>
