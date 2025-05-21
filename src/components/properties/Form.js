3b97aa536be478bb1d0315be1d7935b3787d2622
Prabin Dangol <dprabin23@gmail.com>
2025-05-21 08:47:50 +0545

    complete photo upload
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/components/properties/Form.js
+++ src/components/properties/Form.js
@@ -4,37 +4,42 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import Spinner from "./Spinner";
+import { IoCloudUploadOutline } from "react-icons/io5";
+import Image from "next/image";
 
-function PropertyForm({product, categories}) {
+function PropertyForm({ product, categories }) {
   const [loading, setLoading] = useState(false);
-    const {register, handleSubmit, reset}=useForm({
-        values:product,
-    });
+  const [localImageUrls, setLocalImageUrls] = useState([]);
+  const [productImages, setProductImages] = useState(null);
+  const { register, handleSubmit, reset } = useForm({
+    values: product,
+  });
 
-     async function submitForm(data) {
+  async function submitForm(data) {
     setLoading(true);
     const formData = new FormData();
     formData.append("name", data.name);
     formData.append("brand", data.brand);
     formData.append("price", data.price);
     formData.append("category", data.category);
-   
-
-    if (data.image && data.image[0]) {
-      formData.append("image", data.image[0]); // ✅ Correctly handle FileList
-    }
+    if(data.description) formData.append("description", data.description);
+    
+    productImages.map(image=>{
+      formData.append("images", image);
+    });   
 
     try {
       await createProperty(formData);
       toast.success("Product created successfully!", { autoClose: 750 });
       reset(); // Clear form if needed
     } catch (error) {
-      toast.error(
-        error.response?.data?.message || "Failed to create product",
-        { autoClose: 1000 }
-      );
+      toast.error(error.response?.data?.message || "Failed to create product", {
+        autoClose: 1000,
+      });
     } finally {
       setLoading(false);
+      setProductImages([]);
+      setLocalImageUrls([]);
     }
   }
   return (
@@ -91,11 +96,13 @@ function PropertyForm({product, categories}) {
           />
           <datalist id="categories">
             {categories?.map((category, index) => (
-              <option key={index} value={category}>{category}</option>  
+              <option key={index} value={category}>
+                {category}
+              </option>
             ))}
           </datalist>
         </div>
-        <div className="w-full">
+        <div className="sm:col-span-2">
           <label
             htmlFor="price"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -111,20 +118,6 @@ function PropertyForm({product, categories}) {
             {...register("price")}
           />
         </div>
-        <div>
-          <label
-            htmlFor="file_input"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Upload file
-          </label>
-          <input
-            className="block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            id="file_input"
-            type="file"
-            {...register("image")}
-          />
-        </div>
         <div className="sm:col-span-2">
           <label
             htmlFor="description"
@@ -134,19 +127,72 @@ function PropertyForm({product, categories}) {
           </label>
           <textarea
             id="description"
-            rows={8}
+            rows={4}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             placeholder="Your description here"
             {...register("description")}
           />
         </div>
+
+        <div className="sm:col-span-2 ">
+          <label
+            htmlFor="images"
+            className="flex flex-col items-center justify-center w-full h-40 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+          >
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <IoCloudUploadOutline className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" />
+          
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
+              </p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                SVG, PNG, JPG 
+              </p>
+            </div>
+            <input id="images" type="file" className="hidden" multiple accept=".png, .jpg, .jpeg .svg" onChange={(e)=>{
+              const files = [];
+              const Urls = [];
+               Array.from(e.target.files).map((file) =>{
+                 files.push(file);
+                 Urls.push(URL.createObjectURL(file));
+               });
+              setLocalImageUrls(Urls);
+              setProductImages(files);
+            }} />
+          </label>
+          {localImageUrls.length > 0 && (
+            <div className="flex flex-wrap gap-5 mt-4">
+              {localImageUrls.map((url, index) => (
+                <Image
+                  key={index}
+                  src={url}
+                  alt={`Image ${index}`}
+                  width={100}
+                  height={100}
+                  className="w-20 h-20 object-cover"
+                />
+              ))}
+            </div>
+          )}
+        </div>
       </div>
+
       <button
         type="submit"
-        className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800 cursor-pointer"
+        disabled={loading}
+        className={`inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white ${
+          loading ? "bg-gray-500" : "bg-slate-700 hover:bg-primary-800"
+        } rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 cursor-pointer`}
       >
-       {product?"Edit product":"Add product"}
-       <Spinner />
+        {loading ? (
+          <>
+            Saving...
+            <Spinner />
+          </>
+        ) : (
+          <>{product ? "Edit product" : "Add product"}</>
+        )}
       </button>
     </form>
   );
