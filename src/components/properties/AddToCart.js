50b3eb2c80f51ad84988a62cbbc5245a63f2fd44
Prabin Dangol <dprabin23@gmail.com>
2025-05-25 19:32:45 +0545

    add to card
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

---
+++ src/components/properties/AddToCart.js
@@ -0,0 +1,11 @@
+"use client";
+
+function AddToCart({ property }) {
+    return (
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
+            Add to Cart
+        </button>
+    );
+}
+
+export default AddToCart;
