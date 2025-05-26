80061c411a70cf782a368693dd64cae56b80d875
Prabin Dangol <dprabin23@gmail.com>
2025-05-26 15:48:13 +0545

    complete filter
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/app/properties/page.js
+++ src/app/properties/page.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ProductCard from '@/components/properties/Card';
-import { getBrands, getCategories, getProperties } from '@/api/properties';
-import PropertyDrawer from '@/components/properties/Drawer';
-import PropertyFilters from '@/components/properties/Filters';
+import React from "react";
+import ProductCard from "@/components/properties/Card";
+import { getBrands, getCategories, getProperties } from "@/api/properties";
+import PropertyDrawer from "@/components/properties/Drawer";
+import PropertyFilters from "@/components/properties/Filters";
 
 export const metadata = {
-  title: 'Properties',
+  title: "Properties",
 };
 
-async function PropertiesPage({searchParams}) {
+async function PropertiesPage({ searchParams }) {
   const response = await getProperties(await searchParams);
   const brandsResponse = await getBrands();
   const categoriesresponse = await getCategories();
@@ -16,20 +16,22 @@ async function PropertiesPage({searchParams}) {
   const products = response?.data;
   const brands = brandsResponse?.data;
   const categories = categoriesresponse?.data;
-  
+
   return (
     <section>
-      <h1 className='text-3xl font-bold underline pt-6 text-center'>Popular Properties</h1>
-      <PropertyFilters brands={brands} categories={categories}/>
-    <div className='container grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 py-8 '>
-     {
-      products?.map((product, index)=>(
-        <ProductCard key={index} product={product} />
-      ))
-     }
-      </div> 
-      </section>
-  )
+      <div className="grid grid-cols-[1fr_auto] ">
+        <h1 className="text-3xl font-bold underline pt-6 justify-self-center">
+          Popular Properties
+        </h1>
+        <PropertyFilters brands={brands} categories={categories} />
+        <div className="container grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 py-8 ">
+          {products?.map((product, index) => (
+            <ProductCard key={index} product={product} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default PropertiesPage;
--- src/components/properties/Filters.js
+++ src/components/properties/Filters.js
@@ -1,16 +1,20 @@
 "use client"
 import React, { useState } from 'react'
 import PropertyDrawer from './Drawer';
+import { FiFilter } from "react-icons/fi";
 
 function PropertyFilters({brands, categories}) {
     const [showFilters, setShowFilters] = useState(false);
   return (
     <div className='container '>
-        <button onClick={() => setShowFilters(!showFilters)} className='bg-slate-600 text-white items-center border rounded-lg px-3 py-1 hover:bg-slate-100 hover:text-black'>Filters</button>
+        <button onClick={() => setShowFilters(!showFilters)} 
+        className='bg-slate-600 text-white items-center border flex rounded-lg px-3 py-1 hover:bg-slate-100 hover:text-black'>
+        <span>Filters</span> <FiFilter /></button>
         <PropertyDrawer 
         brands={brands}
         categories={categories}
-        showFilters={showFilters} setShowFilters={setShowFilters}/>   
+        showFilters={showFilters} 
+        setShowFilters={setShowFilters}/>   
     </div>
   )
 }
