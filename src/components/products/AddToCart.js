0d0fa767b8f3207d75eb7841e5915cb3dabe370b
Prabin Dangol <dprabin23@gmail.com>
2025-05-28 06:47:07 +0545

    initial commit

--- src/components/products/AddToCart.js
+++ src/components/products/AddToCart.js
@@ -2,7 +2,7 @@
 
 function AddToCart({ property }) {
     return (
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
+        <button className="px-4 py-2 bg-blue-500 hover:opacity-80 text-white rounded-md">
             Add to Cart
         </button>
     );
